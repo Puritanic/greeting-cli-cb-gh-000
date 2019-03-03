80d0a6d83602bae105f7f382ad8c98d2c889a441
Darko Tasevski <darktasevski@gmail.com>
2019-03-03 10:29:49 +0000

    Automatically backed up by Learn

--- lib/greeting.rb
+++ lib/greeting.rb
@@ -1,6 +1,5 @@
 # code the #greeting method here!
 
 def greeting(name)
-  cleaned = name.delete('\t').delete('\n')
-  puts "Hello #{cleaned}. It's nice to meet you."
+  puts "Hello #{name}. It's nice to meet you."
 end
